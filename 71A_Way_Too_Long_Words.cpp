5e191ff9a3ab830014a6306455071c7a016bb059
techsamagan <156639202+techsamagan@users.noreply.github.com>
2024-02-08 15:02:31 -0600

    Update 71A_Way_Too_Long_Words.cpp

--- 71A_Way_Too_Long_Words.cpp
+++ 71A_Way_Too_Long_Words.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-#include <string>
+#include <string> 
  
 int main() {
     int n;
     std::cin >> n;
  
-    for (int i = 0; i < n; ++i) {
-        std::string word;
-        std::cin >> word;
- 
-        if (word.length() > 10) {
-            std::cout << word[0] << word.length() - 2 << word.back() << std::endl;
-        } else {
-            std::cout << word << std::endl;
+    std::string str[n];
+    
+    for(int i = 0; i < n; i++) {
+        std::cin >> str[i];
+        if(str[i].size() >= 10) { 
+            str[i] = str[i][0] + std::to_string(str[i].size() - 2) + str[i][str[i].size() - 1];
+        }
+    }
+    
+    for(int j = 0; j < n; j++) { 
+        if(j != n-1){
+        std::cout << str[j] << std::endl;
+        }
+        else{
+        std::cout << str[j];
         }
     }
  
