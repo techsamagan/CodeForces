c2a7f84b42da9d7ce574eec59bbdc8dbb1c34f04
techsamagan <156639202+techsamagan@users.noreply.github.com>
2024-02-20 22:58:40 -0600

    Create 1772A_A+B.cpp

---
+++ 1772A_A+B.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int a,b;
+        char c;
+        cin >>a>>c>>b;
+        cout<<a+b<<endl;
+    }
+    return 0;
+}
